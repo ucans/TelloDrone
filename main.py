0ecf19d1402962c9649299e8684bddb41ffe6650
ucans <zyzy1231@naver.com>
2021-11-15 22:35:06 +0900

    Add face recognition

--- main.py
+++ main.py
@@ -6,7 +6,10 @@ w, h = 360, 240
 pError = 0
 
 if __name__ == '__main__':
+    # face_collect()
+
     tello = init_tello()
+    time.sleep(1)
 
     tello.takeoff()
     time.sleep(1)
@@ -15,11 +18,42 @@ if __name__ == '__main__':
     frame_read = tello.get_frame_read()
     time.sleep(2)
 
+    tello.move_up(50)
+    model = learning()
+
     while True:
         img = frame_read.frame
         img = cv.resize(img, (w, h))
-        img, info = detect_face(img)
-        pError = trace_face(tello, info, w, pid, pError)
+        # 얼굴 검출
+        img, info, theRoi = detect_face(img)
+
+        try:
+            theRoi = np.asarray(theRoi)
+            face = cv.cvtColor(theRoi, cv.COLOR_BGR2GRAY)
+            # 예측
+            result = model.predict(face)
+
+            if result[1] < 500:
+                confidence = int(100 * (1 - (result[1]) / 300))
+                display_string = str(confidence) + '% Confidence it is user'
+                cv.putText(img, display_string, (100, 120), cv.FONT_HERSHEY_COMPLEX, 1, (250, 120, 255), 2)
+
+            # 신뢰도가 75보다 크면, 드론이 나를 따라옴.
+            if confidence > 75:
+                cv.putText(img, "Suyeon", (250, 450), cv.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
+                cv.imshow('Face Cropper', img)
+                pError = trace_face(tello, info, w, pid, pError)
+
+            # 신뢰도가 그 밑이면, 드론 가만히 있음.
+            else:
+                cv.putText(img, "Unknown", (250, 450), cv.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 2)
+                cv.imshow('Face Cropper', img)
+
+        except:
+            # 얼굴 검출 안됨
+            cv.putText(img, "Face Not Found", (250, 450), cv.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 2)
+            cv.imshow('Face Cropper', img)
+            pass
 
         print("Center :", info[0], "Area : ", info[1])
         cv.imshow('Output', img)
@@ -35,3 +69,22 @@ if __name__ == '__main__':
         if keyborad == ord('s'): tello.move_back(20)
         if keyborad == ord('a'): tello.move_left(20)
         if keyborad == ord('d'): tello.move_right(20)
+
+    # capture = cv.VideoCapture(cv.CAP_DSHOW + 0)
+    # print('width :%d, height : %d, FPS : %s' % (capture.get(3), capture.get(4), capture.get(4)))
+    #
+    # while capture.isOpened():
+    #     ret, frame = capture.read()
+    #     if ret:
+    #         gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    #         cv.imshow('video', gray)
+    #
+    #         img, info = detect_face(frame)
+    #         cv.imshow('result', img)
+    #
+    #
+    #     if cv.waitKey(1) == ord('q'):
+    #         break
+    #
+    # capture.release()
+    # cv.destroyAllWindows()
--- utils.py
+++ utils.py
@@ -2,9 +2,12 @@ import numpy as np
 from djitellopy import Tello
 import cv2 as cv
 import time
+from os import listdir
+from os.path import isfile, join
 
 fbRange = [6200, 6800]
 pid = [0.4, 0.4, 0]
+
 
 def init_tello():
     tello = Tello()
@@ -33,15 +36,20 @@ def detect_face(img):
     face_cascade = cv.CascadeClassifier('haarcascades_XML/haarcascade_frontalface_default.xml')
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.2, 8)
+    # 1.2 scale factor,
+    # 8 얼굴 사이의 최소 간격 pixel
 
     myFaceListC = []  # Center point
     myFaceListArea = []  # Area
-
+    roi = []
     for (x, y, w, h) in faces:
         cv.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
         cx = x + w // 2
         cy = y + h // 2
         area = w * h
+
+        roi = img[y:y + h, x:x + w]
+        roi = cv.resize(roi, (200, 200))
 
         myFaceListC.append([cx, cy])
         myFaceListArea.append(area)
@@ -50,9 +58,26 @@ def detect_face(img):
 
     if len(myFaceListArea) != 0:
         i = myFaceListArea.index(max(myFaceListArea))
-        return img, [myFaceListC[i], myFaceListArea[i]]
+        return img, [myFaceListC[i], myFaceListArea[i]], roi
     else:
-        return img, [[0, 0], 0]
+        return img, [[0, 0], 0], roi
+
+
+
+# def face_detector(img, size=0.5):
+#     face_classifier = cv.CascadeClassifier('haarcascades_XML/haarcascade_frontalface_default.xml')
+#     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+#     faces = face_classifier.detectMultiScale(gray, 1.3, 5)
+#
+#     if faces is ():
+#         return img, []
+#
+#     for (x, y, w, h) in faces:
+#         cv.rectangle(img, (x, y), (x + w, y + h), (0, 255, 255), 2)
+#         roi = img[y:y + h, x:x + w]
+#         roi = cv.resize(roi, (200, 200))
+#
+#     return img, roi
 
 
 def trace_face(me, info, w, pid, pError):
@@ -79,24 +104,106 @@ def trace_face(me, info, w, pid, pError):
     return error
 
 
-def moveTello(tello):
-    tello.takeoff()
-    time.sleep(5)
+# 전체 사진에서, 얼굴 부위만 추출
+def face_extractor(img):
+    face_classifier = cv.CascadeClassifier('haarcascades_XML/haarcascade_frontalface_default.xml')
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    faces = face_classifier.detectMultiScale(gray, 1.3, 5)
 
-    tello.move_back(50)
-    time.sleep(5)
+    if faces is ():
+        return None
 
-    tello.rotate_clockwise(360)
-    time.sleep(5)
+    for (x, y, w, h) in faces:
+        cropped_face = img[y:y + h, x:x + w]
 
-    tello.move_forward(50)
-    time.sleep(5)
+    return cropped_face
 
-    tello.flip_right()
-    time.sleep(5)
 
-    tello.flip_left()
-    time.sleep(5)
+# 사용자 얼굴 수집
+def face_collect():
+    cap = cv.VideoCapture(0)    # 웹 캠에서 먼저 얼굴 수집
+    count = 0
 
-    tello.land()
-    time.sleep(5)
+    while True:
+        ret, frame = cap.read()
+        if face_extractor(frame) is not None:
+            count += 1
+            # 200 x 200 정사각형 사이즈로
+            face = cv.resize(face_extractor(frame), (200, 200))
+            face = cv.cvtColor(face, cv.COLOR_BGR2GRAY)
+
+            file_name_path = 'Facial-Recognition/faces/user' + str(count) + '.jpg'
+            cv.imwrite(file_name_path, face)
+
+            cv.putText(face, str(count), (50, 50), cv.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
+            cv.imshow('Face Cropper', face)
+        else:
+            print("Face not Found")
+            pass
+
+        if cv.waitKey(1) == 13 or count == 100:
+            break
+
+    cap.release()
+    cv.destroyAllWindows()
+    print('Colleting Samples Complete!!!')
+
+
+# 얼굴 학습
+def learning():
+    data_path = 'Facial-Recognition/faces/'
+    onlyfiles = [f for f in listdir(data_path) if isfile(join(data_path, f))]
+
+    Training_Data, Labels = [], []
+
+    for i, files in enumerate(onlyfiles):
+        image_path = data_path + onlyfiles[i]
+        images = cv.imread(image_path, cv.IMREAD_GRAYSCALE)
+        Training_Data.append(np.asarray(images, dtype=np.uint8))
+        Labels.append(i)
+
+    Labels = np.asarray(Labels, dtype=np.int32)
+    # 모델 생성
+    model = cv.face.LBPHFaceRecognizer_create()
+    # 학슴
+    model.train(np.asarray(Training_Data), np.asarray(Labels))
+
+    print("Model Training Complete!!!!!")
+    return model
+
+
+# while True:
+#
+#     ret, frame = cap.read()
+#     image, face = face_detector(frame)
+#
+#     try:
+#         face = cv.cvtColor(face, cv.COLOR_BGR2GRAY)
+#         result = model.predict(face)
+#
+#         if result[1] < 500:
+#             confidence = int(100 * (1 - (result[1]) / 300))
+#             display_string = str(confidence) + '% Confidence it is user'
+#         cv.putText(image, display_string, (100, 120), cv.FONT_HERSHEY_COMPLEX, 1, (250, 120, 255), 2)
+#
+#         # 신뢰도가 75보다 크면, 드론이 나를 따라옴.
+#         if confidence > 75:
+#             cv2.putText(image, "Unlocked", (250, 450), cv.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
+#             cv2.imshow('Face Cropper', image)
+#
+#         # 신뢰도가 그 밑이면, 드론 가만히 있음.
+#         else:
+#             cv.putText(image, "Locked", (250, 450), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 2)
+#             cv.imshow('Face Cropper', image)
+#
+#
+#     except:
+#         cv.putText(image, "Face Not Found", (250, 450), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 2)
+#         cv.imshow('Face Cropper', image)
+#         pass
+#
+#     if cv.waitKey(1) == 13:
+#         break
+#
+# cap.release()
+# cv.destroyAllWindows()
